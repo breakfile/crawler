33723c9c75f26734132201cabb4b7af7f77d29a3
Harry M.B. Air <121592185+hpmcollins@users.noreply.github.com>
2026-09-23 19:56:17 +1000

    Double resolution

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include "box.h"
 #include <raylib.h>
+#include <string.h>
 
 int main() {
-    InitWindow(400, 300, "engine");
+    InitWindow(800, 600, "engine");
     SetTargetFPS(60);
+
+    RenderTexture r_HQDS = LoadRenderTexture(400, 300);
 
     while (!WindowShouldClose()) {
 
         toupdate();
 
-        BeginDrawing();
+        BeginTextureMode(r_HQDS);
         ClearBackground(RED);
 
         todraw();
 
+        EndTextureMode();
+
+        BeginDrawing();
+        DrawTexturePro(r_HQDS.texture, (Rectangle){0,0,400,-300}, (Rectangle){0,0,800,600}, (Vector2){0,0}, 0, WHITE);
         EndDrawing();
     }
 
